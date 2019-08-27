87f3c21de3b1a426b8ef7992b55c3cc05a7a555e
Fernando Rama <fernandorama.dev@gmail.com>
2019-08-27 19:06:19 -0300

    Add integration simples test

--- crawlers/test/index.test.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('chai');
-
-describe('Crawlers', () => {
-  it('deve retornar ao menos uma hot thread para o subreddit "worldnews"', () => {
-    expect(true).to.deep.include({ subreddit: 'worldnews' });
-  });
-});
