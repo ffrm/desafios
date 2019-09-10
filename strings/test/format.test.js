4e8b069ce3452aaae236e318e00e71940e12a0b6
Fernando Rama <fernandorama.dev@gmail.com>
2019-09-10 19:21:17 -0300

    Prevent multiple spaces and unecessary breaklines at bottom of text

--- strings/test/format.test.js
+++ strings/test/format.test.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { expect } = require('chai');
 const IdwallFormatter = require('../');
 
-const readFile = (file) => fs.readFileSync(file).toString();
+const readFile = (file) => fs.readFileSync(file, { encoding: 'utf-8' }).toString();
 
 const inputText = readFile(path.resolve(__dirname, 'input.txt'));
 const expectedOutput1 = readFile(path.resolve(__dirname, 'output', 'wrapped.txt'));
@@ -26,5 +26,17 @@ describe('Manipulação de strings', () => {
 
   it('deve retornar o texto justificado a direita quando apenas uma palavra', () => {
     expect(idwallFormatter.format('a', 3, true)).to.equal('  a\n');
+  });
+
+  it('deve retornar texto ignorando múltiplos espaços', () => {
+    const input = 'Lorem  Ipsum  is  simply';
+    const output = 'Lorem Ipsum is simply\n';
+    expect(idwallFormatter.format(input, 28, false)).to.equal(output);
+  });
+
+  it('deve retornar texto ignorando quebras de linha no fim do mesmo', () => {
+    const input = 'Lorem  Ipsum  is  simply\n\n:digito:\n\n';
+    const output = 'Lorem Ipsum is simply\n\n:digito:\n';
+    expect(idwallFormatter.format(input, 28, false)).to.equal(output);
   });
 });
